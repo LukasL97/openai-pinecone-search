18e20fe5e036cf930ccb785b42b86caf717fc9e0
Lukas Lehmann <lukas.lehmann97@gmail.com>
2023-11-06 09:50:29 +0100

    download wikitexts

---
+++ wikitext.py
@@ -0,0 +1,16 @@
+
+import os
+from datasets import load_dataset
+
+dataset = load_dataset("wikipedia", "20220301.en", split="train", streaming=True)
+dataset = dataset.take(100)
+
+output_directory = "data"
+os.makedirs(output_directory, exist_ok=True)
+
+for i, row in enumerate(dataset):
+    title = row['title']
+    text = row['text']
+    file_path = os.path.join(output_directory, f"{title}.txt")
+    with open(file_path, 'w', encoding='utf-8') as file:
+        file.write(text)
